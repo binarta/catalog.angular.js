0f77d75fe3adb85b1632a4c9988eefe823e28926
Jo Kerkhofs <jke@thinkerit.be>
2015-08-21 20:29:39 +0200

    Added catalog unavailable templates

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,18 @@ var minifyHtmlOpts = {
     quotes: true
 };
 
-gulp.task('templates-bootstrap3', function () {
-    gulp.src('template/bootstrap3/**/*.html')
+gulp.task('catalog-bootstrap3', function () {
+    gulp.src('template/bootstrap3/*.html')
         .pipe(minifyHtml(minifyHtmlOpts))
         .pipe(templateCache('catalog-tpls-bootstrap3.js', {standalone: true, module: 'catalog.templates'}))
         .pipe(gulp.dest('src'));
 });
 
-gulp.task('default', ['templates-bootstrap3']);
+gulp.task('catalog-unavailable-bootstrap3', function () {
+    gulp.src('template/bootstrap3/unavailable/*.html')
+        .pipe(minifyHtml(minifyHtmlOpts))
+        .pipe(templateCache('catalog-unavailable-tpls-bootstrap3.js', {standalone: true, module: 'catalog.templates'}))
+        .pipe(gulp.dest('src'));
+});
+
+gulp.task('default', ['catalog-bootstrap3', 'catalog-unavailable-bootstrap3']);
